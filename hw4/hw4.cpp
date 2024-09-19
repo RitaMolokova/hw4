bcc1690863a701f34eec8b0e3dfde83c5311d5d5
Molokova Rita <Molok_cv58@student.itstep.org>
2024-09-19 21:33:11 +0300

    Добавьте файлы проекта.

---
+++ hw4/hw4.cpp
@@ -0,0 +1,35 @@
+﻿using namespace std;
+#include <iostream>
+#include <random>
+
+class fraction {
+private:
+    int a, b;
+public:
+    void Print() {
+        cout << "Result (a/b): " << a / b << endl;
+    }
+    void Input(int c, int d) {
+        a = c;
+        b = d;
+    }
+    void InputRandom() {
+        srand(time(0));
+        a = 1 + rand() % (100 - 1 + 1);
+        b = 1 + rand() % (100 - 1 + 1);
+    }
+};
+
+int main()
+{
+    fraction c;
+    c.Input(4, 2);
+    c.Print();
+
+    fraction d;
+    d.InputRandom();
+    d.Print();
+}
+
+
+
